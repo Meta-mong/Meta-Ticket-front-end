072c9b7ebdc6c2ed7a3d3fe75b61be8f8d0b0d29
leesh125 <chelsea12345@naver.com>
2022-06-04 17:23:29 +0900

    add Ranking div(please check "a:focus" !)

--- ticketOpenPage.js
+++ ticketOpenPage.js
@@ -3,11 +3,9 @@ let number = page.innerText;
 
 function btn_prev() {
     number = (parseInt(number)%2) + 1;
-    console.log(number);
     page.innerText = number;
 }
 function btn_next() {
     number = (parseInt(number)%2) + 1;
-    console.log(number);
     page.innerText = number;
 }
