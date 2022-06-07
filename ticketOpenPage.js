f41feda97a59d7ef216cc707d80ec1e2ef952a8f
leesh125 <chelsea12345@naver.com>
2022-06-07 10:54:01 +0900

    update disable btn

--- ticketOpenPage.js
+++ ticketOpenPage.js
@@ -2,10 +2,30 @@ const page = document.getElementById('ticket-open-page');
 let number = page.innerText;
 
 function btn_prev() {
-    number = (parseInt(number)%2) + 1;
-    page.innerText = number;
+    var btn = document.querySelector(".slick-prev");
+    btn.addEventListener("click", function(e) {
+        var vm = this;
+        this.setAttribute("disabled", "disabled");
+
+        setTimeout(function () {
+            vm.removeAttribute("disabled")
+        }, 800)
+        number = (parseInt(number)%2) + 1;
+        page.innerText = number;
+    })
+    
 }
 function btn_next() {
-    number = (parseInt(number)%2) + 1;
-    page.innerText = number;
+    var btn2 = document.querySelector(".slick-next");
+    btn2.addEventListener("click", function(e) {
+        var vm2 = this;
+        this.setAttribute("disabled", "disabled");
+
+        setTimeout(function () {
+            vm2.removeAttribute("disabled")
+        }, 800)
+        number = (parseInt(number)%2) + 1;
+        page.innerText = number;
+    })
+    
 }
