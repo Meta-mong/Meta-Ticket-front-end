bc6b2262ba2bd8582b3733c5bf4ea2970c0d299d
leesh125 <chelsea12345@naver.com>
2022-06-07 11:13:32 +0900

    ReUpdate disable btn

--- ticketOpenPage.js
+++ ticketOpenPage.js
@@ -1,31 +1,33 @@
 const page = document.getElementById('ticket-open-page');
 let number = page.innerText;
 
-function btn_prev() {
-    var btn = document.querySelector(".slick-prev");
-    btn.addEventListener("click", function(e) {
-        var vm = this;
-        this.setAttribute("disabled", "disabled");
 
-        setTimeout(function () {
-            vm.removeAttribute("disabled")
-        }, 800)
-        number = (parseInt(number)%2) + 1;
-        page.innerText = number;
-    })
+function btn_prev() {   
+    const btn_prev = document.querySelector(".slick-prev");
+    const btn_next = document.querySelector(".slick-next");
+    
+    btn_prev.setAttribute("disabled", "disabled");
+    btn_next.setAttribute("disabled", "disabled");
+    number = (parseInt(number)%2) + 1;
+    page.innerText = number;
+    console.log(page.innerText);
+    setTimeout(function () {
+        btn_prev.removeAttribute("disabled");
+        btn_next.removeAttribute("disabled");
+    }, 600)
     
 }
 function btn_next() {
-    var btn2 = document.querySelector(".slick-next");
-    btn2.addEventListener("click", function(e) {
-        var vm2 = this;
-        this.setAttribute("disabled", "disabled");
-
-        setTimeout(function () {
-            vm2.removeAttribute("disabled")
-        }, 800)
-        number = (parseInt(number)%2) + 1;
-        page.innerText = number;
-    })
+    const btn_prev = document.querySelector(".slick-prev");
+    const btn_next = document.querySelector(".slick-next");
     
+    btn_prev.setAttribute("disabled", "disabled");
+    btn_next.setAttribute("disabled", "disabled");
+    number = (parseInt(number)%2) + 1;
+    page.innerText = number;
+    console.log(page.innerText);
+    setTimeout(function () {
+        btn_prev.removeAttribute("disabled");
+        btn_next.removeAttribute("disabled");
+    }, 600)
 }
